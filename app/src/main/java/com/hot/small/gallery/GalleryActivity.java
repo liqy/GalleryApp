19d775f3d40df06067b6cf8373aba7ca90cf55e5
liqy <liqingyi@outlook.com>
2016-07-12 18:03:19 +0800

    增加处理库

---
+++ app/src/main/java/com/hot/small/gallery/GalleryActivity.java
@@ -0,0 +1,16 @@
+package com.hot.small.gallery;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * 图片表格
+ */
+public class GalleryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gallery);
+    }
+}
---
+++ app/src/main/java/com/hot/small/gallery/PreviewActivity.java
@@ -0,0 +1,16 @@
+package com.hot.small.gallery;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * 图片预览
+ */
+public class PreviewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_preview);
+    }
+}
